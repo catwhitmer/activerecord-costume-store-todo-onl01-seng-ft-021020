1aad0dd472c46ad981ae815788479ebbfa6a6218
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-17 18:06:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStore < ActiveRecord::Migration[5.2]
         t.string "name"
         t.string "location"
         t.integer "costume_inventory"
-        t.string "image url"
+        t.integer "number_of_employees"
         t.timestamp "created_at"
         t.timestamp "updated_at"
     end
