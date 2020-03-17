49b8993373f8469dffc4c7fc36cf251eacb8ef6e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-17 17:59:54 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,7 +8,7 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-class CreateToppings < ActiveRecord::Migration[5.2]
+class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :toppings do |t|
         t.string "name"
