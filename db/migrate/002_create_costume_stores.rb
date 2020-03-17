4dee3270a19f91b30ec9afd950274618cc2cd600
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-17 18:05:12 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStore < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+        t.string "name"
+        t.integer "price"
+        t.integer "size"
+        t.string "image url"
+        t.timestamp "created_at"
+        t.timestamp "updated_at"
+    end
+  end
+end
