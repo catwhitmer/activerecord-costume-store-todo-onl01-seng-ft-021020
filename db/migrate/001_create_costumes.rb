11410b5cfcc7d82da464d68ca7b0a0357f6b1b11
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-17 18:47:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,10 +11,10 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
-        t.string :"name"
-        t.float "price"
-        t.string "size"
-        t.string "image url"
+        t.string :name
+        t.float :price
+        t.string :size
+        t.string :image_url
         t.timestamps
     end
   end
