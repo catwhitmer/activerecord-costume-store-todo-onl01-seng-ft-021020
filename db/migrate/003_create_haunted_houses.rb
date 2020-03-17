febc38ea92146cfda22e30501dba9dcfb3a5389c
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-17 18:10:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+        t.string "name"
+        t.string "location"
+        t.integer "theme"
+        t.boolean "price"
+        t.timestamp "family_friendly"
+        t.timestamp "opening_date"
+        t.timestamp "closing_date"
+        t.string "long_description"
+    end
+  end
+end
