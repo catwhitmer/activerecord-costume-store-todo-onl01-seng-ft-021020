ad59bea7907cb97bd0365c2343f15e68e676e278
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-17 18:00:16 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,7 +10,7 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    create_table :toppings do |t|
+    create_table :costumes do |t|
         t.string "name"
         t.boolean "vegetable"
         t.integer "pizza_id"
